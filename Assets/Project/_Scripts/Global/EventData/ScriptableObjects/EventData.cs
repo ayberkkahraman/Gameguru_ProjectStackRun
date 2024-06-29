0440479f6bbe8b5bdb7eb9bf69a98903b5e796ae
Ayberk Kahraman <ayberkkahraman99@gmail.com>
2024-06-29 14:43:09 +0300

    #update
    +Project Initialization

---
+++ Assets/Project/_Scripts/Global/EventData/ScriptableObjects/EventData.cs
@@ -0,0 +1,11 @@
+﻿using Project._Scripts.Global.Interfaces;
+using UnityEngine;
+
+namespace Project._Scripts.Global.EventData.ScriptableObjects
+{
+  public abstract class EventData : ScriptableObject, IDisposer
+  {
+    public abstract void Execute();
+    public abstract void Dispose();
+  }
+}
