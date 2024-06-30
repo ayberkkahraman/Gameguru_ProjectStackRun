d52cb6e0f84c99fa7be61e97dc1bc043a5d15546
Ayberk Kahraman <ayberkkahraman99@gmail.com>
2024-06-30 16:34:17 +0300

    #update
    +Platform Scale System has been added

--- Assets/Project/_Scripts/GameCore/PlatformSystem/Core/Platform.cs
+++ Assets/Project/_Scripts/GameCore/PlatformSystem/Core/Platform.cs
@@ -1,62 +1,87 @@
 ﻿using DG.Tweening;
 using Project._Scripts.GameCore.PlatformSystem.System;
-using Project._Scripts.Global.Manager.Core;
 using UnityEngine;
 
 namespace Project._Scripts.GameCore.PlatformSystem.Core
 {
   public class Platform : MonoBehaviour
   {
+    private Rigidbody _rigidbody;
+    private BoxCollider _boxCollider;
+
+    private const float SnapDuration = .05f;
     [Range(1f,5f)]public float TransitionDuration = 2f;
-    private Tweener _transitionTween;
-
-    private void OnEnable()
-    {
-      // ManagerCore.Instance.GetInstance<PlatformController>().CurrentPlatform = this;
-      PlatformController.OnPlatformKilledHandler += (_) =>
-      {
-        _transitionTween.Kill();
-        SlicePlatform();
-      };
-    }
-
-    private void OnDisable()
-    {
-      PlatformController.OnPlatformKilledHandler -= (_) =>
-      {
-        _transitionTween.Kill();
-        SlicePlatform();
-      };
-    }
+    public Tweener TransitionTween;
+    
 
     private void Start()
     {
-      _transitionTween = transform.DOMoveX(-transform.position.x, TransitionDuration)
+      TransitionTween = transform.DOMoveX(-transform.position.x, TransitionDuration)
         .SetEase(Ease.Linear)
         .SetLoops(-2, LoopType.Yoyo);
     }
 
-    private void SlicePlatform()
+    public void SnapPlatform()
     {
-      float currentPlatformPositionX = transform.position.x;
-      float previousPlatformPositionX = ManagerCore.Instance.GetInstance<PlatformController>().PreviousPlatform.transform.position.x;
+      transform.DOMoveX(PlatformController.PreviousPlatform.position.x, SnapDuration)
+        .SetEase(Ease.InOutSine)
+        .OnComplete(() => PlatformController.OnPlatformSnappedHandler(this));
+    }
 
-      var distance = currentPlatformPositionX - previousPlatformPositionX;
-      int multiplier = distance >= 0 ? 1 : -1;
-
+    public void RescalePlatform(float distance)
+    {
       transform.localScale = new Vector3(transform.localScale.x - Mathf.Abs(distance), transform.localScale.y, transform.localScale.z);
       transform.position = new Vector3(transform.position.x - (distance / 2), transform.position.y, transform.position.z);
-      SpawnDropCube(distance);
+    }
+    
+    public void KillPlatform()
+    {
+      var distance = GetDistanceFromPlatform();
+
+      if (Mathf.Abs(distance) <= PlatformController.TOLERANCE)
+      {
+        SnapPlatform();
+      }
+
+      else
+      {
+        PlatformController.SnappedPlatformCount = 0;
+        RescalePlatform(distance);
+        SpawnDropCube(distance);
+      }
+      
+      PlatformController.OnPlatformSpawnedHandler();
+    }
+
+    private float GetDistanceFromPlatform()
+    {
+      float currentPlatformPositionX = transform.position.x;
+      float previousPlatformPositionX = PlatformController.PreviousPlatform.transform.position.x;
+      return currentPlatformPositionX - previousPlatformPositionX;
     }
 
     private void SpawnDropCube(float differenceXPosition)
     {
       var platform = GameObject.CreatePrimitive(PrimitiveType.Cube);
       int multiplier = differenceXPosition >= 0 ? 1 : -1;
-      platform.transform.localScale = new Vector3(differenceXPosition, transform.localScale.y, transform.localScale.z);
-      Transform previousPlatform = ManagerCore.Instance.GetInstance<PlatformController>().PreviousPlatform.transform;
-      platform.transform.position = new Vector3((previousPlatform.localScale.x/2 + multiplier*differenceXPosition/2)*multiplier, transform.position.y, transform.position.z);
+      platform.transform.localScale = new Vector3(Mathf.Abs(differenceXPosition), transform.localScale.y, transform.localScale.z);
+      platform.transform.position = new Vector3(transform.position.x + multiplier*transform.localScale.x/2 + (differenceXPosition/2), transform.position.y, transform.position.z);
 
+      platform.AddComponent<Rigidbody>();
+      
+      
+      Destroy(this);
+    }
+
+    public void IncreaseScale()
+    {
+      var currentScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+      var scaleTreshold = .25f;
+      var scaleAmount = currentScale.x < currentScale.z ? (currentScale.z - currentScale.x > scaleTreshold ? scaleTreshold : currentScale.z - currentScale.x) : 0f;
+      Debug.Log(scaleAmount);
+      if(Mathf.Approximately(currentScale.x, currentScale.z)) return;
+      currentScale.x += scaleAmount;
+      transform.localScale = currentScale;
     }
   }
 }
--- Assets/Project/_Scripts/GameCore/PlatformSystem/System/PlatformController.cs
+++ Assets/Project/_Scripts/GameCore/PlatformSystem/System/PlatformController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DG.Tweening;
 using Project._Scripts.GameCore.PlatformSystem.Core;
 using UnityEngine;
 
@@ -6,31 +7,92 @@ namespace Project._Scripts.GameCore.PlatformSystem.System
 {
   public class PlatformController : MonoBehaviour
   {
-    public Platform CurrentPlatform;
-    public Platform PreviousPlatform;
+    public Platform PlatformPrefab;
+    public static Platform CurrentPlatform;
+    public static Transform PreviousPlatform;
+
+    [Range(0f, 2f)] public float Tolerance = .1f;
+    public static float TOLERANCE;
+
+    public static int SnappedPlatformCount;
+
+    public delegate void OnPlatformSnapped(Platform platform);
+    public static OnPlatformSnapped OnPlatformSnappedHandler;
    
    public delegate void OnPlatformKilled(Platform platform);
    public static OnPlatformKilled OnPlatformKilledHandler;
    
-   public delegate void OnPlatformSpawned(Platform platform);
+   public delegate void OnPlatformSpawned();
    public static OnPlatformSpawned OnPlatformSpawnedHandler;
 
-   private void Start()
+   private int _platformCount;
+
+   private void Start() => OnPlatformSpawnedHandler();
+
+   private void Awake() => TOLERANCE = Tolerance;
+
+   private void OnEnable()
    {
-     PreviousPlatform = GameObject.Find("Platform_2").GetComponent<Platform>();
+     SnappedPlatformCount = 0;
+     
+     OnPlatformSpawnedHandler += SpawnPlatform;
+     OnPlatformKilledHandler += KillPlatform;
+
+     OnPlatformSnappedHandler += (_) => IncreaseSnappedPlatformCount();
+     OnPlatformSnappedHandler += CheckSnappedPlatforms;
+     OnPlatformSnappedHandler += (_) => PlayAudio();
    }
 
    private void OnDisable()
    {
-     OnPlatformKilledHandler = null;
+     OnPlatformKilledHandler -= KillPlatform;
+     OnPlatformSpawnedHandler -= SpawnPlatform;
    }
 
    private void Update()
    {
-     if (Input.GetMouseButtonDown(0))
-     {
-       OnPlatformKilledHandler(CurrentPlatform);
-     }
+     if (!Input.GetMouseButtonDown(0))
+       return;
+     OnPlatformKilledHandler(CurrentPlatform);
+   }
+
+   private void KillPlatform(Platform platform)
+   {
+     platform.TransitionTween.Kill();
+     platform.KillPlatform();
+   }
+
+   private void IncreaseSnappedPlatformCount() => SnappedPlatformCount++;
+   private void CheckSnappedPlatforms(Platform platform)
+   {
+     platform.IncreaseScale();
+     // if(Mathf.Approximately(platform.transform.localScale.x, platform.transform.localScale.z)) return;
+     
+     // if (SnappedPlatformCount >= 3)
+     // {
+     //   platform.IncreaseScale();
+     // }
+   }
+
+   private void PlayAudio()
+   {
+     // Debug.Log("Music Note");
+   }
+
+   private void SpawnPlatform()
+   {
+     PreviousPlatform = transform.GetChild(0);
+     int multiplier = _platformCount % 2 == 0 ? 1 : -1;
+     var position = new Vector3(multiplier * -6f, PreviousPlatform.transform.position.y, PreviousPlatform.transform.position.z + PreviousPlatform.transform.localScale.z);
+     
+     Platform platform = Instantiate(PlatformPrefab, position, Quaternion.identity, transform);
+     platform.transform.localScale = PreviousPlatform.localScale;
+
+     platform.transform.SetSiblingIndex(0);
+     PreviousPlatform = transform.GetChild(1);
+     CurrentPlatform = platform;
+
+     _platformCount++;
    }
   }
 }
